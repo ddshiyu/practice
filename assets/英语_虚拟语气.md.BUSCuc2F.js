147f41a16bb4a047f552e4f5669e83f8caabd3d7
ddshiyu <ddshiyu@users.noreply.github.com>
2025-04-19 06:03:05 +0000

    deploy: 87293c5820a74c8079d85bba43685a7e12187d20

---
+++ assets/英语_虚拟语气.md.BUSCuc2F.js
@@ -0,0 +1,58 @@
+import{_ as a,c as e,o as t,aq as o}from"./chunks/framework.ATjo20Wq.js";const r="/assets/image2.CN18dAOe.avif",m=JSON.parse('{"title":"虚拟语气","description":"","frontmatter":{},"headers":[],"relativePath":"英语/虚拟语气.md","filePath":"英语/虚拟语气.md","lastUpdated":1745042540000}'),h={name:"英语/虚拟语气.md"},n=o(`<h1 id="虚拟语气" tabindex="-1">虚拟语气 <a class="header-anchor" href="#虚拟语气" aria-label="Permalink to &quot;虚拟语气&quot;">​</a></h1><p>英语<strong>语气</strong>主要包括三种</p><h2 id="陈述语气" tabindex="-1">陈述语气 <a class="header-anchor" href="#陈述语气" aria-label="Permalink to &quot;陈述语气&quot;">​</a></h2><pre><code>描述现实，“这兔子吃了这胡萝卜。 The rabbit ate the carrot.”
+</code></pre><h2 id="祈使语气" tabindex="-1">祈使语气 <a class="header-anchor" href="#祈使语气" aria-label="Permalink to &quot;祈使语气&quot;">​</a></h2><pre><code>表示命令、请求，“兔子，吃这胡萝卜！Rabbit, eat the carrot!”
+
+绝大多数情况下，祈使语气就是用第二人称（“你”）用一般现在时表达，然后把“你”去掉，并保证动词原形就好。
+
+如，You eat the carrot! -&gt; Eat the carrot!
+</code></pre><h2 id="虚拟语气-1" tabindex="-1">虚拟语气 <a class="header-anchor" href="#虚拟语气-1" aria-label="Permalink to &quot;虚拟语气&quot;">​</a></h2><pre><code>动词需要变位。
+
+英语在描述动作/事件这一方面的思维习惯是：
+
+如果描述脑中的想象，尤其是和事实相反的，或者说“主观的意愿”，那么就要使用虚拟语气。
+
+如果是描述现实，包括直接描述请求命令，就用陈述、祈使语气。
+
+虚拟语气用法之一：表不可能的假设
+
+1. 与现在事实相反
+
+如果我现在看到那只兔子的话，我会咬他。
+
+If I see the rabbit now, I will bite him.【×】
+
+Now：现在，很明确的时间
+
+will bite：很确信在将来发生的事情。
+
+事实不是这样，所以把时间往过去推一个时间段，变成 
+
+If I saw the rabbit now, I would bite him.【虚拟语气的难点】
+
+这里的过去式并不表示过去时间，不是在描述过去的事实，而是在描述现在的、没发生，但是希望发生的事，是一个主观意愿或者说是想象的假设。
+
+If I see the rabbit tomorrow, I will bite him.【条件语气】
+
+
+2. 与将来事实相反 or 可能性很小
+
+【兔子去找嫦娥了。（找不到了）】
+
+万一（可能性小，想象而已）我明天看见那只兔子，我要咬他。
+
+If I should see the rabbit tomorrow, I would bite him.
+
+构成：条件句：should + 动词原形；主句：would+动词原形
+
+
+3. 与过去事实相反
+
+以后都找不到兔子了，现在后悔没在刚出院的时候咬兔子。
+
+我要是前几天看见兔子，我就咬他
+
+If I had seen the rabbit a few days ago, I would have bitten him.
+
+假设对过去某一点有影响，所以用“完成态”
+
+构成：条件句：had+动词过去分词；主句：would have+动词过去分词
+</code></pre><h2 id="虚拟语气有两大类" tabindex="-1">虚拟语气有两大类 <a class="header-anchor" href="#虚拟语气有两大类" aria-label="Permalink to &quot;虚拟语气有两大类&quot;">​</a></h2><h3 id="第一大用法-表-不可能-假设-与事实相反的假设" tabindex="-1">第一大用法：表“不可能”假设（与事实相反的假设） <a class="header-anchor" href="#第一大用法-表-不可能-假设-与事实相反的假设" aria-label="Permalink to &quot;第一大用法：表“不可能”假设（与事实相反的假设）&quot;">​</a></h3><h4 id="_1-与现在相反" tabindex="-1">1. 与现在相反 <a class="header-anchor" href="#_1-与现在相反" aria-label="Permalink to &quot;1. 与现在相反&quot;">​</a></h4><p>条件句：if + 过去式;</p><p>主句： would + 动词原形</p><p>If I knew it, I would tell you.【如果我现在知道（其实我不知道），我现在会告诉你】</p><p>*如果动词是be，要用were，不管什么人称。</p><p>If I were you, I would give this video a thumbs-up.如果我是你，我会给这视频点赞。</p><h4 id="_2-与将来相反-or-可能性很小" tabindex="-1">2. 与将来相反 or 可能性很小 <a class="header-anchor" href="#_2-与将来相反-or-可能性很小" aria-label="Permalink to &quot;2. 与将来相反 or 可能性很小&quot;">​</a></h4><p>条件句：if + should/were to + 动词原形;</p><p>主句：would/should/could/might + 动词原形;</p><p>If should meet thee after long years, how should I greet thee? 【万一我们将来重逢了，我又该如何面对你呢？】</p><h4 id="_3-与过去相反" tabindex="-1">3. 与过去相反 <a class="header-anchor" href="#_3-与过去相反" aria-label="Permalink to &quot;3. 与过去相反&quot;">​</a></h4><p>条件句：if + had+ 动词过去分词;</p><p>主句：would have + 动词过去分词</p><p>If I had watched this video before, I would have aced my test.【我要是之前就看了这视频，那绝对能考个好成绩】</p><p>言下之意：我没看过这个视频，我并没有考出好成绩。</p><h3 id="第二大用法-表达愿望、请求、建议、命令-希望某个动作-事件发生" tabindex="-1">第二大用法：表达愿望、请求、建议、命令（希望某个动作/事件发生） <a class="header-anchor" href="#第二大用法-表达愿望、请求、建议、命令-希望某个动作-事件发生" aria-label="Permalink to &quot;第二大用法：表达愿望、请求、建议、命令（希望某个动作/事件发生）&quot;">​</a></h3><h4 id="_1-wish-希望可能性小的事儿发生" tabindex="-1">1. Wish 希望可能性小的事儿发生 <a class="header-anchor" href="#_1-wish-希望可能性小的事儿发生" aria-label="Permalink to &quot;1. Wish 希望可能性小的事儿发生&quot;">​</a></h4><p>（1）与现在相反</p><p>wish + 过去式</p><p>I(主语) wish(谓语动词) that I were a rabbit. 我希望我是只兔子。</p><p>that 引导的从句做句子的宾语，也就是一个宾语从句，that可以省略，I wish I were a rabbit.</p><p>（2）与将来相反</p><p>wish + would/should/could/might + 动词原型</p><p>I wish (that) I could eat the carrot. 我希望我能吃那根胡萝卜。（我多半吃不到）</p><p>（3）与过去相反</p><p>wish + 过去完成式</p><p>I wish (that) I had eaten the carrot. 我希望我吃了那根胡萝卜。（我没吃，后悔）</p><h4 id="_2-demand-要求-suggest-建议-insist-坚持-order-命令-would-rather-should-动词原形" tabindex="-1">2. Demand（要求）, suggest（建议）, insist（坚持）,order（命令）, would rather + (should)+动词原形 <a class="header-anchor" href="#_2-demand-要求-suggest-建议-insist-坚持-order-命令-would-rather-should-动词原形" aria-label="Permalink to &quot;2. Demand（要求）, suggest（建议）, insist（坚持）,order（命令）, would rather + (should)+动词原形&quot;">​</a></h4><p>I would rather that you didn’t eat the carrot. 我宁愿你不会吃那根胡萝卜。【现在】</p><p>would rather后接虚拟语气，表达现在的愿望，意思就是我宁愿你现在，或者说接下来不要吃那根胡萝卜，言下之意：你还没吃它，我就是希望你不要吃。</p><p>I would rather that you hadn’t eaten the carrot. 我宁愿你没吃掉那根胡萝卜。【过去】</p><p>言下之意：你已经吃掉了，我就是妄想一下</p><p>I demand /suggest/ insist/order that you (should) give me a carrot. 我要求/建议/坚持/命令你给我一根胡萝卜。</p><p>I demanded /suggested/ insisted/ordered that you (should) give me a carrot. 我之前要求/建议/坚持/命令你给我一根胡萝卜。</p><p>I suggest that you (should) eat a carrot. 宾语从句</p><p>It is suggested that you (should) eat a carrot. 主语从句</p><p>The suggestion is that you (should) eat a carrot. 表语从句</p><p>It is (high) time that you ate a carrot. 定语从句 【high在这里表示早该这么做了】这里的ate 也是虚拟语气</p><p><img src="`+r+'" alt=""></p>',49),i=[n];function d(s,l,u,p,c,b){return t(),e("div",null,i)}const I=a(h,[["render",d]]);export{m as __pageData,I as default};
